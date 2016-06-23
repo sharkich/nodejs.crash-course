c71012585313ed9b5ae567596b06565671665455
Artem Koziar <a@temich.in.ua>
2016-06-23 19:27:43 +0300

    02: working setInterval

--- 02.streams-buffers/index.js
+++ 02.streams-buffers/index.js
@@ -10,16 +10,23 @@
 const fs = require('fs');
 const stream = require('stream');
 
-const reader = new fs.createReadStream('1.txt', {
+const reader = new fs.createReadStream('./02.streams-buffers/1.txt', {
   encode: 'utf-8'
 });
 const writer = process.stdout;
 
 const transformer = new stream.Transform();
 
-transformer._transform = (chank, enc, cb) => {
-  transformer.push(chank.toString().toUpperCase());
-  cb();
+transformer._transform = (chunk, enc, cb) => {
+  let chunkArray = chunk.toString().toUpperCase().split('\n');
+  let interval = setInterval(() => {
+    let el = chunkArray.shift();
+    transformer.push(el + '\n');
+    if (!chunkArray.length) {
+      clearInterval(interval);
+      return cb();
+    }
+  }, 1000);
 };
 
 reader.pipe(transformer).pipe(writer);
